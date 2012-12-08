2676abb33aaffd42fdf15782f9508ef8c577c2ed
Tom Allen <tom@jugglethis.net>
2012-12-09 10:20:59 +1100

    Create new rails app - WikiBlog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikiblog::Application.config.secret_token = '164d3b02a4ff7fb387f18c39bfcb4574aedb0534bd58d5e4ff981904c3dc5f72053c117634227386d423a7ff7cb66edb9ae19be5c09348a61dac1651f565936a'
